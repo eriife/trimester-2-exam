e9ced3ddb6b2d0a39625c3740d8f662c3b7dd002
eriife <70342972+eriife@users.noreply.github.com>
2021-02-25 13:09:15 -0500

    Add files via upload

---
+++ comp sci trimester 2 project/js/script.js
@@ -0,0 +1,12 @@
+var city = document.getElementById('city');
+var rainy = document.getElementById('rainy');
+var ocean = document.getElementById('ocean');
+
+
+function myFunction() {
+  alert('put headphones on for optimal zen experience');
+}
+
+
+
+myFunction();
